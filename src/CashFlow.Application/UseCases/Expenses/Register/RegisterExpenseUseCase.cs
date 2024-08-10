b2b26eda80a63544caf429f9e0a94d6338efc3ea
Lucas Magnago de Oliveira <lucas.magnago.02@gmail.com>
2024-08-10 17:23:07 -0300

    Validando despesas usando if

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,24 @@ namespace CashFlow.Application.UseCases.Expenses.Register
     {
         public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
         {
-            // TO DO VALIDATIONS
+            Validate(request);
 
             return new ResponseRegisterExpenseJson();
+        }
+
+        private void Validate(RequestRegisterExpenseJson request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Title))
+                throw new ArgumentException("The title is required.");
+
+            if (request.Amount <= 0)
+                throw new ArgumentException("The Amount must be greater than zero.");
+
+            if (DateTime.Compare(request.Date, DateTime.UtcNow) > 0)
+                throw new ArgumentException("Expenses cannot be for the future");
+
+            if (Enum.IsDefined(typeof(PaymentType), request.PaymentType) == false)
+                throw new ArgumentException("Payment Type is not valid.");
         }
     }
 }
