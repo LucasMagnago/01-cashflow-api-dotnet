d4ce28a60ea0048574b435659e11dc39fd7eb594
Lucas Magnago de Oliveira <lucas.magnago.02@gmail.com>
2024-08-11 11:29:50 -0300

    Configurando o Entity Framework

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CashFlow.Domain.Entities;
+
+namespace CashFlow.Infrastructure.DataAccess
+{
+    public class CashFlowDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=dev@123;";
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 39));
+
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+    }
+}
