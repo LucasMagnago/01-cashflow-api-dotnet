a16e1aa8a1deece694441a03e6680c7a6f3a4ec5
Lucas Magnago de Oliveira <lucas.magnago.02@gmail.com>
2024-08-25 18:24:40 -0300

    Princípio O do SOLID: Open-Closed

--- src/CashFlow.Exception/ExceptionsBase/CashFlowException.cs
+++ src/CashFlow.Exception/ExceptionsBase/CashFlowException.cs
@@ -2,9 +2,13 @@
 {
     public abstract class CashFlowException : SystemException
     {
+        public abstract int StatusCode { get; }
+
         protected CashFlowException(string message) : base(message)
         {
 
         }
+
+        public abstract List<string> GetErrors();
     }
 }
