1179b7e34de948ec170e6a638e5532b832456d60
Lucas Magnago de Oliveira <lucas.magnago.02@gmail.com>
2025-03-02 10:50:28 -0300

    Criptografando a senha para o banco de dados

--- src/CashFlow.Application/AutoMapper/AutoMapping.cs
+++ src/CashFlow.Application/AutoMapper/AutoMapping.cs
@@ -16,7 +16,8 @@ namespace CashFlow.Application.AutoMapper
         private void RequestToEntity()
         {
             CreateMap<RequestExpenseJson, Expense>();
-            CreateMap<RequestRegisterUserJson, User>();
+            CreateMap<RequestRegisterUserJson, User>()
+                .ForMember(entityUser => entityUser.Password, config => config.Ignore());
         }
 
         private void EntityToResponse()
---
+++ src/CashFlow.Domain/Security/Cryptography/IPasswordEncripter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Domain.Security.Cryptography
+{
+    public interface IPasswordEncripter
+    {
+        string Encrypt(string password);
+    }
+}
