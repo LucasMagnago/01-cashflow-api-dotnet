fde4b218ab8f25e8efd1fc4471ccc59cb9f308d2
Lucas Magnago de Oliveira <lucas.magnago.02@gmail.com>
2025-03-02 10:50:00 -0300

    Validator para senha

--- src/CashFlow.Application/UseCases/Users/PasswordValidator.cs
+++ src/CashFlow.Application/UseCases/Users/PasswordValidator.cs
@@ -4,17 +4,64 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CashFlow.Application.UseCases.Users
 {
-    public class PasswordValidator<T> : PropertyValidator<T, string>
+    public partial class PasswordValidator<T> : PropertyValidator<T, string>
     {
+        public string ERROR_MESSAGE_KEY = "ErrorMessage";
         public override string Name => "PasswordValidator";
 
         public override bool IsValid(ValidationContext<T> context, string password)
         {
-            return false;
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, "Sua senha deve ter no mínimo 8 caracteres, contendo pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial (por exemplo, !, ?, *, .).");
+                return false;
+            }
+
+            if (password.Length < 8)
+            {
+                context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, "Sua senha deve ter no mínimo 8 caracteres, contendo pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial (por exemplo, !, ?, *, .).");
+                return false;
+            }
+
+            if (!LowerCaseLetter().IsMatch(password))
+            {
+                context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, "Sua senha deve ter no mínimo 8 caracteres, contendo pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial (por exemplo, !, ?, *, .).");
+                return false;
+            }
+
+            if (!UpperCaseLetter().IsMatch(password))
+            {
+                context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, "Sua senha deve ter no mínimo 8 caracteres, contendo pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial (por exemplo, !, ?, *, .).");
+                return false;
+            }
+
+            if (!Numbers().IsMatch(password))
+            {
+                context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, "Sua senha deve ter no mínimo 8 caracteres, contendo pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial (por exemplo, !, ?, *, .).");
+                return false;
+            }
+
+            if (!SpecialSymbols().IsMatch(password))
+            {
+                context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, "Sua senha deve ter no mínimo 8 caracteres, contendo pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial (por exemplo, !, ?, *, .).");
+                return false;
+            }
+
+            return true;
         }
+
+        [GeneratedRegex(@"[a-z]+")]
+        private static partial Regex LowerCaseLetter();
+        [GeneratedRegex(@"[A-Z]+")]
+        private static partial Regex UpperCaseLetter();
+        [GeneratedRegex(@"[0-9]+")]
+        private static partial Regex Numbers();
+        [GeneratedRegex(@"[\!\?\*\.]+")]
+        private static partial Regex SpecialSymbols();
     }
 }
