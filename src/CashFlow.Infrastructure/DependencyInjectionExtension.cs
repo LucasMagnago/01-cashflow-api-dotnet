f7059e4abe3e4ec9b1dc7a36be82f9534eccd7a4
Lucas Magnago de Oliveira <lucas.magnago.02@gmail.com>
2025-03-02 23:26:41 -0300

    Implementando o token JWT

--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -2,8 +2,10 @@
 using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Domain.Repositories.User;
 using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Domain.Security.Tokens;
 using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.DataAccess.Repositories;
+using CashFlow.Infrastructure.Security.Tokens;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,20 +17,21 @@ namespace CashFlow.Infrastructure
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             AddDbContext(services, configuration);
+            AddToken(services, configuration);
             AddRepositories(services);
 
-            services.AddScoped<IPasswordEncripter, Security.BCrypt>();
+            services.AddScoped<IPasswordEncripter, Security.Cryptography.BCrypt>();
         }
 
-        public static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+        private static void AddToken(IServiceCollection services, IConfiguration configuration)
         {
-            var connectionString = configuration.GetConnectionString("Connection");
-            var serverVersion = new MySqlServerVersion(new Version(8, 0, 39));
+            var expirationTimeMinutes = configuration.GetValue<uint>("Settings:Jwt:ExpirationTimeInMinutes");
+            var signingKey = configuration.GetValue<string>("Settings:Jwt:SigningKey");
 
-            services.AddDbContext<CashFlowDbContext>(config => config.UseMySql(connectionString, serverVersion));
+            services.AddScoped<IAccessTokenGenerator>(config => new JwtTokenGenerator(expirationTimeMinutes, signingKey!));
         }
 
-        public static void AddRepositories(IServiceCollection services)
+        private static void AddRepositories(IServiceCollection services)
         {
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<IExpensesReadOnlyRepository, ExpensesRepository>();
@@ -36,6 +39,14 @@ namespace CashFlow.Infrastructure
             services.AddScoped<IExpensesUpdateOnlyRepository, ExpensesRepository>();
             services.AddScoped<IUserReadOnlyRepository, UserRepository>();
             services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+        }
+
+        private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Connection");
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 39));
+
+            services.AddDbContext<CashFlowDbContext>(config => config.UseMySql(connectionString, serverVersion));
         }
     }
 }
